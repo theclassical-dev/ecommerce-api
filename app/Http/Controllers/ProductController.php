dfedb1dacf79aa36ea3c6331908f31c470d8d7c9
Abraham Peter <theclassical001@gmail.com>
2022-06-21 19:05:37 +0100

    product controller + model + migration file added

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Response;
+use Auth;
+use File;
+use App\Models\User;
+
+class ProductController extends Controller
+{
+    //
+}
