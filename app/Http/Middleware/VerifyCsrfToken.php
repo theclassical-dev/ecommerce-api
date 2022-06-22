223a7203ab27224abe4e17de1423763bfbfc2e04
Abraham Peter <theclassical001@gmail.com>
2022-06-22 14:35:54 +0100

    csrf middleware updated 2

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'https://ecommerces-api.herokuapp.com/api/*'
+        'https://ecommerces-api.herokuapp.com/api/register'
     ];
 }
