df0d96ea3adf09d613b222f3d4c6460943101bc4
Abraham Peter <theclassical001@gmail.com>
2022-06-24 17:54:50 +0100

    #update 9

--- database/migrations/2022_06_21_180358_create_products_table.php
+++ database/migrations/2022_06_21_180358_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('price');
             $table->string('discount');
             $table->string('discription');
