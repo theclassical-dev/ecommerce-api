dc82d7204e53aa5f0c22012c94ffd58619426655
Abraham Peter <theclassical001@gmail.com>
2022-07-04 13:29:09 +0100

    test resource added + updates

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\LoginResource;
 
 class Authcontroller extends Controller
 {
